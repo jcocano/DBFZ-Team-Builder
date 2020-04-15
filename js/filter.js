c7389f666582e28cfc9f95191900f3fa2792414b
Jesus Cocaño <jesus.cocano@gmail.com>
2020-04-15 11:45:01 -0500

    color-schema

---
+++ js/filter.js
@@ -0,0 +1,21 @@
+
+var filtro2 = document.getElementById("praza")
+var filtro3 = document.getElementById("pdlc")
+
+var lista = document.getElementById("li")
+console.log(lista)
+
+function rolDePersonaje(){
+    //variables
+    var filtro1, ul, li, a, i, textvalue;
+    filtro1 = document.getElementById("ptype");
+    ul = document.getElementById("selector");
+    li = ul.getElementsByTagName('li');
+
+    //prueba
+    console.log(li)
+    
+
+}
+
+
