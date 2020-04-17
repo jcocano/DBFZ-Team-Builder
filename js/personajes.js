60ad45db63c15c9a1dd7d961254260aeebb3ab64
Jesus Cocaño <jesus.cocano@gmail.com>
2020-04-16 23:08:34 -0500

    fixed ,

--- js/personajes.js
+++ js/personajes.js
@@ -1,95 +1,96 @@
-cosnt PERSONAJES = [
+let PERSONAJES = [
     {
-        id: 01
-        nombre: "Androide 16"
-        movimiento1: "Dynamite Driver"
-        movimiento2: "Flying Slide Powerbomb"
-        movimiento3: "Hell Heat"
-        super1: "Hell Flash"
-        super2: "Hell Flash: Full Power"
-        super3: "The Final Power that shouldn't be used "
-        asistencia_a: "Hell Heat"
-        asistencia_b: "Hell Impact"
-        asistencia_c: "Hyper Tackle"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_16"
+        id: 01,
+        nombre: "Androide 16",
+        movimiento1: "Dynamite Driver",
+        movimiento2: "Flying Slide Powerbomb",
+        movimiento3: "Hell Heat",
+        super1: "Hell Flash",
+        super2: "Hell Flash: Full Power",
+        super3: "The Final Power that shouldn't be used ",
+        asistencia_a: "Hell Heat",
+        asistencia_b: "Hell Impact",
+        asistencia_c: "Hyper Tackle",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_16",
         image: "../icos/android-16.png"
 
-    }
+    },
     {
-        id: 02
-        nombre: "Androide 17" 
-        movimiento1: "Accel Driver"
-        movimiento2: "Acrobatic Assault"
-        movimiento3: "Finishing Driver"
-        movimiento4: "Power Blitz "
-        super1: "Barrier Explosion"
-        super2: "Endgame"
-        super3: "Super Electric Strike"
-        asistencia_a: "Reverse Gear"
-        asistencia_b: "Power Blitz Charge"
-        asistencia_c: "Accel Driver"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_17"
+        id: 02,
+        nombre: "Androide 17" ,
+        movimiento1: "Accel Driver",
+        movimiento2: "Acrobatic Assault",
+        movimiento3: "Finishing Driver",
+        movimiento4: "Power Blitz ",
+        super1: "Barrier Explosion",
+        super2: "Endgame",
+        super3: "Super Electric Strike",
+        asistencia_a: "Reverse Gear",
+        asistencia_b: "Power Blitz Charge",
+        asistencia_c: "Accel Driver",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_17",
         image: "../icos/android-17.png"
-    }
+    },
     {
-        id: 03
-        nombre: "Androide 18"
-        movimiento1: "Back Grapple"
-        movimiento2: "Support Attack"
-        movimiento3: "Destructo-Disc"
-        super1: "Energy Wave"
-        super2: "Accel Dance"
-        asistencia_a: "Barrier"
-        asistencia_b: "Destructo-Disc"
-        asistencia_c: "Support Attack"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_18"
+        id: 03,
+        nombre: "Androide 18",
+        movimiento1: "Back Grapple",
+        movimiento2: "Support Attack",
+        movimiento3: "Destructo-Disc",
+        super1: "Energy Wave",
+        super2: "Accel Dance",
+        asistencia_a: "Barrier",
+        asistencia_b: "Destructo-Disc",
+        asistencia_c: "Support Attack",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_18",
         image: "../icos/android-18"
-    }
+    },
     {
-        id: 04
-        nombre: "Androide 21"
-        movimiento1: "Total Detonation Ball"
-        movimiento2: "Hors d'Oeuvre Stab"
-        movimiento3: "Connoisseur Cut"
-        super1: "Photon Wave"
-        super2: "Sweet Tooth"
-        super3: "Excellent Full Course"
-        asistencia_a: "Total Detonation Ball"
-        asistencia_b: "Hors d'Oeuvre Stab"
-        asistencia_c: "Snack Time"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_21"
+        id: 04,
+        nombre: "Androide 21",
+        movimiento1: "Total Detonation Ball",
+        movimiento2: "Hors d'Oeuvre Stab",
+        movimiento3: "Connoisseur Cut",
+        super1: "Photon Wave",
+        super2: "Sweet Tooth",
+        super3: "Excellent Full Course",
+        asistencia_a: "Total Detonation Ball",
+        asistencia_b: "Hors d'Oeuvre Stab",
+        asistencia_c: "Snack Time",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Android_21",
         image: "../icos/android-21.png"
-    }
+    },
     {
-        id: 05
-        nombre: "Bardock"
-        movimiento1: "Rebellion Spear"
-        movimiento2: "Raging Meteor"
-        movimiento3: "Tyrant Lancer"
-        super1: "Riot Javelin"
-        super2: "Saiyan Spirit"
-        super3: "Revenge Assault"
-        asistencia_a: "Rebellion Spear"
-        asistencia_b: "Tyrant Lancer"
-        asistencia_c: "Rebellion Combination"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Bardock"
+        id: 05,
+        nombre: "Bardock",
+        movimiento1: "Rebellion Spear",
+        movimiento2: "Raging Meteor",
+        movimiento3: "Tyrant Lancer",
+        super1: "Riot Javelin",
+        super2: "Saiyan Spirit",
+        super3: "Revenge Assault",
+        asistencia_a: "Rebellion Spear",
+        asistencia_b: "Tyrant Lancer",
+        asistencia_c: "Rebellion Combination",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Bardock",
         image: "../icos/bardock"
-    }
+    },
     {
-        id: 05
-        nombre: "Goku Base"
-        movimiento1: "Heavy Elbow"
-        movimiento2: "Back Throw"
-        movimiento3: "Kamehameha"
-        movimiento4: "Everyone, lend me your energy!"
-        movimiento5: "Henkidama"
-        super1: "Kaioken"
-        super2: "Super Henkidama"
-        asistencia_a: "Energy Shockwave"
-        asistencia_b: "Everyone, lend me your energy!"
-        asistencia_c: "Kamehameha"
-        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Goku"
+        id: 05,
+        nombre: "Goku Base",
+        movimiento1: "Heavy Elbow",
+        movimiento2: "Back Throw",
+        movimiento3: "Kamehameha",
+        movimiento4: "Everyone, lend me your energy!",
+        movimiento5: "Henkidama",
+        super1: "Kaioken",
+        super2: "Super Henkidama",
+        asistencia_a: "Energy Shockwave",
+        asistencia_b: "Everyone, lend me your energy!",
+        asistencia_c: "Kamehameha",
+        url: "https://www.dustloop.com/wiki/index.php?title=DBFZ/Goku",
         image: "../icos/base-goku.png"
-    }
+    },
+    
 
 ]
